58ed5a4a75ab2ecdcf9787d3d023b2e371c4675b
Mark100F <Lezvie94@gmail.com>
2019-08-12 08:47:27 +0300

    Linux start

--- unicom24/src/test/java/hooks/Hooks.java
+++ unicom24/src/test/java/hooks/Hooks.java
@@ -3,15 +3,24 @@ package hooks;
 import base.Base;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.apache.commons.lang3.SystemUtils;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import properties.BaseProperties;
 
 public class Hooks extends Base {
 
     @Before
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", BaseProperties.driverDir );
-        driver = new ChromeDriver();
+        if (SystemUtils.IS_OS_LINUX) {
+            System.setProperty("webdriver.chrome.driver", BaseProperties.driverDirLinux );
+
+        } else {
+            System.setProperty("webdriver.chrome.driver", BaseProperties.driverDir);
+        }
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
     }
 
