5e7dda042e1bbdb7bdf94925768a16e40c44b62f
Mark100F <Lezvie94@gmail.com>
2019-08-28 12:37:07 +0300

    Добавил красивый отчет

--- unicom24/src/test/java/runners/TestFirstRunner.java
+++ unicom24/src/test/java/runners/TestFirstRunner.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
         strict = true,
         snippets = SnippetType.CAMELCASE,
         monochrome = true,
+        format = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"},
         plugin = "ru.yandex.qatools.allure.cucumberjvm.AllureReporter"
 )
 public class TestFirstRunner extends Base {
