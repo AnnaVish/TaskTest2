46c1286da9a6bc927382b1bd794953c1c5a40794
VALROY <valroy17@yandex.ru>
2020-01-22 23:56:28 +0300

    QA-101 Декомпозировать автотест - проверка истории запросов кредитных отчетов после создания запроса

--- unicom24/src/test/java/stepDefinitions/FullCreditHistoryPageStepDefinitions.java
+++ unicom24/src/test/java/stepDefinitions/FullCreditHistoryPageStepDefinitions.java
@@ -3,7 +3,11 @@ package stepDefinitions;
 import base.Base;
 import cucumber.api.java.ru.Тогда;
 import org.junit.Assert;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.TimeoutException;
 import pages.FullCreditHistoryPage;
+import pages.FullCreditsHistoryNewPage;
 import properties.BaseProperties;
 
 import java.io.File;
@@ -12,27 +16,27 @@ public class FullCreditHistoryPageStepDefinitions extends Base {
 
     private final BaseProperties baseProperties = new BaseProperties();
     private final FullCreditHistoryPage fullCreditHistoryPage = new FullCreditHistoryPage();
+    private final FullCreditsHistoryNewPage fullCreditsHistoryNewPage = new FullCreditsHistoryNewPage();
 
-    @Тогда("^отображается страница Полная кредитная история$")
-    public void financialHealtPageIsDisplayed(){
-        fullCreditHistoryPage.pageIsDisplayed();
-    }
+    @Тогда("^проверка страницы Полная кредитная история$")
+    public void financialHealthPageIsDisplayed(){
+        // тут будет проверка что за страница
+        try {// это старая страница
+            fullCreditHistoryPage.pageIsDisplayed();
+            fullCreditHistoryPage.getReportClick();
+            fullCreditHistoryPage.seeReportResult();
+            Base.refreshPage();
+            fullCreditHistoryPage.seeReportResult();
+            fullCreditHistoryPage.downloadReport();
+            Assert.assertTrue(folderSize(new File(baseProperties.createDownloadDirURL())) > 300000);
+            Assert.assertEquals("pdf", getFileExtension(new File(baseProperties.createDownloadDirURL())));
+        }//тут новая страница
+        catch (NoSuchElementException|TimeoutException| StaleElementReferenceException e) {
+            fullCreditsHistoryNewPage.pageIsDisplayed();
+            fullCreditsHistoryNewPage.DownloadBlankBtnClick();
+            Assert.assertTrue(folderSize(new File(baseProperties.createDownloadDirURL())) > 60000);
+            Assert.assertEquals("pdf", getFileExtension(new File(baseProperties.createDownloadDirURL())));
 
-    @Тогда("^пользователь нажимает получить отчет - Полная кредитная история$")
-    public void userClickGetReport(){
-        fullCreditHistoryPage.getReportClick();
-    }
-
-    @Тогда("^пользователь видит что отчет по полной кредитной истории создался$")
-    public void userSeeCreatedResult(){
-        fullCreditHistoryPage.seeReportResult();
-    }
-
-
-    @Тогда("^пользователь скачивает отчет Полная кредитная история$")
-    public void userDownloadReport(){
-        fullCreditHistoryPage.downloadReport();
-        Assert.assertTrue(folderSize(new File(baseProperties.createDownloadDirURL())) > 300000);
-        Assert.assertEquals("pdf", getFileExtension(new File(baseProperties.createDownloadDirURL())));
+        }
     }
 }
