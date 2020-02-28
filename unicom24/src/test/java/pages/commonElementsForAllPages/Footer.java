dd0ef1fa6584a4e281e9fa263ebdcb651d1feaf5
Голубев Дмитрий <golubev.dv@unicom24.ru>
2020-02-28 12:53:43 +0000

    Update Footer.java

--- unicom24/src/test/java/pages/commonElementsForAllPages/Footer.java
+++ unicom24/src/test/java/pages/commonElementsForAllPages/Footer.java
@@ -85,7 +85,6 @@ public class Footer {
                 cabinetOfBank, cabinetOfWebmaster,
                 copyright, copyrightText, becomeAgentDown, becomePartnerDown,
                 personalData, mail, siteMap, adress, aboutCompany);
-
     }
 
 }
