8868609149a61df1815bb0559a30730e1fc4bee9
VALROY <valroy17@yandex.ru>
2020-04-06 18:16:10 +0300

    QA-245 Фикс Смс

--- unicom24/src/test/java/pages/SelectionPage.java
+++ unicom24/src/test/java/pages/SelectionPage.java
@@ -6,17 +6,17 @@
  */
 package pages;
 
-import TestContext.TestContext;
 import base.Base;
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import pages.commonElementsForAllPages.Footer;
 import pages.commonElementsForAllPages.Header;
 import pages.commonElementsForAllPages.UserData;
+import pages.mock.SmsCatcher;
 import pagesUrls.PagesUrls;
+import testContext.TestContext;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,6 +24,7 @@ import java.util.List;
 public class SelectionPage extends Base {
     Header headerPage = new Header();
     Footer footerPage = new Footer();
+    SmsCatcher smsCatcher = new SmsCatcher();
 
     @FindBy(xpath = "//h1[contains(text(), 'Персональный подбор')]")
     private WebElement selectionTitle;
@@ -49,9 +50,7 @@ public class SelectionPage extends Base {
     private final List<WebElement>elements;
 
     public SelectionPage(){
-        PageFactory.initElements(driver, headerPage);
         PageFactory.initElements(driver, this);
-        PageFactory.initElements(driver, footerPage);
         elements = Arrays.asList(selectionTitle, selectionForm, selectionBTN);
     }
 
@@ -94,56 +93,15 @@ public class SelectionPage extends Base {
     }
 
     public void InputSMSCodeForSelection() {
-        try {
-            openNewTab();
-            switchToTheSecondTab();
-            for (int i = 0; i < 4; i++) {
-                if (TestContext.passwordFromSms == null) {
-                    try {
-                        getPasswordSMSFromServer(PagesUrls.smsServerLink2().get("smsServer1"));
-                    } catch (Exception e) {
-                        getPasswordSMSFromServer(PagesUrls.smsServerLink2().get("smsServer2"));
-                    }
-                } else break;
-            }
-            driver.close();
-            switchToTheFirstTab();
+        smsCatcher.getPasswordSMSFromServer(SmsCatcher.smsServerLink().get("smsServer"));
+        driver.close();
+        switchToTheFirstTab();
+        clearField(fieldForCodeFromSMS);
+        while (!fieldForCodeFromSMS.getAttribute("value").equals(TestContext.passwordFromSms)) {
             clearField(fieldForCodeFromSMS);
-            //typeIntoField(TestContext.passwordFromSms, passwordField); иногда теряет 1 символ
-            // Повторение не ведомой хрени, заставляем селениум писать в поле ящик до тех пор пока не напишет правильно
-            while (!fieldForCodeFromSMS.getAttribute("value").equals(TestContext.passwordFromSms)) {
-                clearField(fieldForCodeFromSMS);
-                typeIntoField(TestContext.passwordFromSms, fieldForCodeFromSMS);
-            }
-            selectionNextBTNClick();
-            //waitForUrlContains(PagesUrls.baseUrl() + "/");
+            typeIntoField(TestContext.passwordFromSms, fieldForCodeFromSMS);
         }
-        catch (Exception e) {
-            openNewTab();
-            switchToTheSecondTab();
-            getPasswordSMSFromServer(PagesUrls.smsServerLink2().get("smsServer2"));
-            driver.close();
-            switchToTheFirstTab();
-            clearField(fieldForCodeFromSMS);
-            //typeIntoField(TestContext.passwordFromSms, passwordField); иногда теряет 1 символ
-            // Повторение не ведомой хрени, заставляем селениум писать в поле ящик до тех пор пока не напишет правильно
-            while (!fieldForCodeFromSMS.getAttribute("value").equals(TestContext.passwordFromSms)) {
-                clearField(fieldForCodeFromSMS);
-                typeIntoField(TestContext.passwordFromSms, fieldForCodeFromSMS);
-            }
-
-            selectionNextBTNClick();
-            //waitForUrlContains(PagesUrls.baseUrl() + "/");
-        }
-    }
-
-    public void getPasswordSMSFromServer(String url) {
-        driver.get(url);
-        String xPath = String.format("//tr[./td[contains(text(), '%s')]]/td[contains(text(), 'Код')]", UserData.getFormatNumber());
-        TestContext.passwordFromSms = driver.findElement(By.xpath(xPath)).getText();
-        TestContext.passwordFromSms = TestContext.passwordFromSms.replaceAll("[^0-9]", "");
-        TestContext.passwordFromSms = TestContext.passwordFromSms.substring(0, TestContext.passwordFromSms.length() - 2);
-        TestContext.smsServerValueUrl = driver.getCurrentUrl();
+        selectionNextBTNClick();
     }
 
 }
